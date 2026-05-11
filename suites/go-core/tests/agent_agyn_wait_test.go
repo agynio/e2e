9bc81f6ee49477f29e7825aafd3470fc0cb385bf
Jules Vega <jules.vega@agyn.io>
2026-05-11 17:53:29 +0000

    test(agents): scope agyn wait diagnostics labels

--- suites/go-core/tests/agent_agyn_wait_test.go
+++ suites/go-core/tests/agent_agyn_wait_test.go
@@ -87,9 +87,9 @@ func TestAgentAgynCLIWaitToAnotherAgent(t *testing.T) {
 	sentMessage := sendMessage(t, threadsCtx, threadsClient, threadAID, identityID, prompt)
 	sentMessageTime := messageCreatedAt(t, sentMessage)
 	labels := map[string]string{
-		labelManagedBy: managedByValue,
-		labelAgentID:   agentAID,
-		labelThreadID:  threadAID,
+		"label." + labelManagedBy: managedByValue,
+		"label." + labelAgentID:   agentAID,
+		"label." + labelThreadID:  threadAID,
 	}
 	t.Cleanup(func() {
 		ids, err := findWorkloadsByLabels(ctx, runnerClient, labels)
